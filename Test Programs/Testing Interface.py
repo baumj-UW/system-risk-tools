1b46f6bb07604580a5d815edd4679fa408ed0b37
baumj-UW <45178573+baumj-UW@users.noreply.github.com>
2019-10-25 18:17:23 -0700

    Recreate table 3.13 and plot

---
+++ Test Programs/Testing Interface.py
@@ -0,0 +1,11 @@
+"""
+Created by Jackie Baum on 10/17/2019
+
+Project Description:
+This is a collection of tests and demos
+"""
+
+import scipy
+from pypower.api import *
+
+sysdata = loadcase(casefile=case9())
